ae01c3ceced69b19ab6fd012a79ea99208a09683
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-11-23 16:50:42 +0000

    generate template complete

--- src/generate-template.js
+++ src/generate-template.js
@@ -75,4 +75,34 @@ const generateTemplate = (teamMembers) => {
     return team.join("");
   };
 
-  
+  module.exports = (teamMembers) => {
+    return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>The Team</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css">
+      <script src="https://kit.fontawesome.com/bcd4d79a5a.js"></script>
+  </head>
+  <body>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-12 jumbotron mb-4 team-heading bg-primary">
+                  <h1 class="text-center text-white">The Team</h1>
+              </div>
+          </div>
+      </div>
+      <div class="container">
+          <div class="row">
+              <div class="row team-area col-12 d-flex justify-content-center">
+                  ${generateTemplate(teamMembers)}
+              </div>
+          </div>
+      </div>
+  </body>
+  </html>
+      `;
+  };
